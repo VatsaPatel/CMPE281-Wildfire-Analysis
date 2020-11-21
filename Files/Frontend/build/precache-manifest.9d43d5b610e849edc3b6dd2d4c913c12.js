1bbe6083c98c62cca3ea1e36981d4903d8786e0e
Vatsa <vatsapatel13@gmail.com>
2020-11-20 18:39:02 -0800

    update

---
+++ Files/Frontend/build/precache-manifest.9d43d5b610e849edc3b6dd2d4c913c12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3491c84c19f30f4feeb687cec21b25ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28dff6bd49eef784664b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7c5ae5f7b95e796f2a0e",
+    "url": "/static/js/2.5740fd85.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.5740fd85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28dff6bd49eef784664b",
+    "url": "/static/js/main.0ebc44f4.chunk.js"
+  },
+  {
+    "revision": "7ffd9d1eb6e939e83634",
+    "url": "/static/js/runtime-main.74e1d82b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
